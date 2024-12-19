3ae97a2dc8a7610cb44f2d8b8e3f085f2a0db94d
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-19 12:27:14 +0900

    feat : "RentalLogRepository"

--- src/main/java/com/example/demo/rentallog/repository/RentalLogRepository.java
+++ src/main/java/com/example/demo/rentallog/repository/RentalLogRepository.java
@@ -1,6 +1,6 @@
-package com.example.demo.repository;
+package com.example.demo.rentallog.repository;
 
-import com.example.demo.entity.RentalLog;
+import com.example.demo.rentallog.entity.RentalLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
