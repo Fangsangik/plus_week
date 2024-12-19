bac9db5a2015a0aa1054fa9900516ff352198649
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-19 12:28:32 +0900

    feat : "ReservationRepository"

--- src/main/java/com/example/demo/reservation/repository/ReservationRepository.java
+++ src/main/java/com/example/demo/reservation/repository/ReservationRepository.java
@@ -1,6 +1,6 @@
-package com.example.demo.repository;
+package com.example.demo.reservation.repository;
 
-import com.example.demo.entity.Reservation;
+import com.example.demo.reservation.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,10 +8,12 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Query("select r from Reservation r join fetch r.user u join fetch r.item i")
+    List<Reservation> findAllReservationsWithJoin();
 
     List<Reservation> findByUserIdAndItemId(Long userId, Long itemId);
 
